8e87072ae3ec6bc6b9bc4e6ea0dd8c07d8308dec
Saran-pariyar <spariyar05@gmail.com>
2023-05-14 14:22:34 +0545

    Added route.js code inside auth folder

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google"
+import { signIn, signOut } from "next-auth/react";
+
+const handler = NextAuth({
+
+    providers : [
+        GoogleProvider({
+            clientId: '',
+            clientSecret : "",
+        })
+
+    ],
+    async session({session}){
+
+    },
+    async signIn({profile }){
+
+    }
+})
+
+export {handler as GET, handler as POST}
