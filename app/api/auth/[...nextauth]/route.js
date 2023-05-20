5b9ff07abea7d3716f841f7b3525d9b5ce85987d
Saran-pariyar <spariyar05@gmail.com>
2023-05-20 21:25:53 +0545

    wrote code for signIn()

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,9 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
 import { signIn, signOut } from "next-auth/react";
 
+//importing this function from utils/database.js
+import { connectionToDB } from "@utils/database";
+import User from '@models/user';
 
 
 const handler = NextAuth({
@@ -17,14 +20,34 @@ const handler = NextAuth({
 
     },
     async signIn({ profile }) {
-        try { 
+        try {
 
-            
+            await connectionToDB()
+
+            //check if user already exist
+            const userExists = await User.findOne({ email: profile.email });
+
+            //if user doesn't exist, create one 
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    //we did this so that name doesn't have any spaces and it is in lowercase
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture,
+                });
+            }
+            //return true if signIn successful
+            return true;
         }
-        catch (error) { }
+        catch (error) {
+            //return false and console.log the error if failed to signIn
+            console.log(error);
+            return false
+        }
     }
 })
 
 export { handler as GET, handler as POST }
 
 // console page: https://console.cloud.google.com/apis/credentials?hl=en-AU&organizationId=0&project=long-walker-387007
+//second : https://cloud.mongodb.com/v2/6465d0094a3d6c0854dc0881#/clusters
