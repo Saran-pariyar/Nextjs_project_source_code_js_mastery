b38bac9338888115e8d2d8823ae3509140777b30
Saran-pariyar <spariyar05@gmail.com>
2023-05-20 21:31:49 +0545

    Added code to store session in route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -17,7 +17,11 @@ const handler = NextAuth({
 
     ],
     async session({ session }) {
+        // store the user id from MongoDB to session
+        const sessionUser = await User.findOne({ email: session.user.email });
+        session.user.id = sessionUser._id.toString();
 
+        return session;
     },
     async signIn({ profile }) {
         try {
